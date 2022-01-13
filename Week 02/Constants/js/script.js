d0307bea41e7ad974d8480f636cb3cd4fe488b2d
ipioselli <ines.pioselli@gmail.com>
2022-01-13 18:03:45 -0500

    week02:Finished video 2.1

--- Week 02/Constants/js/script.js
+++ Week 02/Constants/js/script.js
@@ -2,16 +2,21 @@
 Ines Pioselli
 2.1 Constants
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+
 */
 
 "use strict";
 
+const NUM_CIRCLES = 10;
+let circleAlpha = 50;
+let circleSizeIncrease = 50;
 
-/**
-Description of preload
-*/
+
+const PI = 3.14159; //constants cannot change value
+
+const MY_FAVOURITE_PROGRAMMING_LANGUAGE = `Javascript`;
+
+
 function preload() {
 
 }
@@ -21,6 +26,7 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas(500,500);
 
 }
 
@@ -29,5 +35,16 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(0);
+
+  circleAlpha = map(mouseX, 0, width, 10, 100);
+  circleSizeIncrease = map(mouseY, 0, height, 10, 100);
+
+  for(let i=0; i <NUM_CIRCLES; i++){
+    push();
+    fill(255,circleAlpha);
+    ellipse(width/2, height/2,i*circleSizeIncrease);
+    pop();
+  }
 
 }
