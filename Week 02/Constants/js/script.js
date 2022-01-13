92d70a68e563dda0477b48650347848e4efd1c22
ipioselli <ines.pioselli@gmail.com>
2022-01-13 17:34:00 -0500

    Week 02: Start video 2.1

---
+++ Week 02/Constants/js/script.js
@@ -0,0 +1,33 @@
+/**
+Ines Pioselli
+2.1 Constants
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
