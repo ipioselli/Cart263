144d946a57d28b62cc2a4dc1d436378791a9aa80
ipioselli <ines.pioselli@gmail.com>
2022-01-16 13:01:32 -0500

    started video 2.2

---
+++ Week 02/Object Parameters/js/script.js
@@ -0,0 +1,32 @@
+/**
+Ines Pioselli
+2.2 Object Parameters
+
+
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
