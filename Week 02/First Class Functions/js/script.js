93a8dd98f259349760e54d6d3b155f5e8c99369a
ipioselli <ines.pioselli@gmail.com>
2022-01-16 14:16:51 -0500

    Finished video 2.3

--- Week 02/First Class Functions/js/script.js
+++ Week 02/First Class Functions/js/script.js
@@ -7,26 +7,35 @@ Ines Pioselli
 
 "use strict";
 
+let addingFunction = add;
 
-/**
-Description of preload
-*/
-function preload() {
+let result = addingFunction(1,10);
 
-}
+//alert(`The result is ${result}`);
+
+// function hello(){
+//   alert(`Hello!`);
+// }
+
+// let hello = function(){
+//   alert(`Hello!`);
+// };
+
+setTimeout(function(){  //adds a delay
+  alert(`Hello!`);
+}, 5000);
 
 
-/**
-Description of setup
-*/
+
 function setup() {
 
 }
 
 
-/**
-Description of draw()
-*/
 function draw() {
 
+}
+
+function add(a,b){
+  return a + b;
 }
