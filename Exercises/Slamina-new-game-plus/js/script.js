1b8fc0b8d7d81a2038351cf520be5159d2b81fc0
ipioselli <ines.pioselli@gmail.com>
2022-01-27 18:11:05 -0500

    E2: Added fruit variables

--- Exercises/Slamina-new-game-plus/js/script.js
+++ Exercises/Slamina-new-game-plus/js/script.js
@@ -1,150 +1,114 @@
 /**
 Activity 2: Slamina
 Ines Pioselli
+Brief:
+- add start and end screens
+- add visuals when you get an answer right or wrong
+- add sound effects
+- add a counter
+- add different words
 
 */
 
 "use strict";
 
-const animals = [
-  "aardvark",
-  "alligator",
-  "alpaca",
-  "antelope",
-  "ape",
-  "armadillo",
-  "baboon",
-  "badger",
-  "bat",
-  "bear",
-  "beaver",
-  "bison",
-  "boar",
-  "buffalo",
-  "bull",
-  "camel",
-  "canary",
-  "capybara",
-  "cat",
-  "chameleon",
-  "cheetah",
-  "chimpanzee",
-  "chinchilla",
-  "chipmunk",
-  "cougar",
-  "cow",
-  "coyote",
-  "crocodile",
-  "crow",
-  "deer",
-  "dingo",
-  "dog",
-  "donkey",
-  "dromedary",
-  "elephant",
-  "elk",
-  "ewe",
-  "ferret",
-  "finch",
-  "fish",
-  "fox",
-  "frog",
-  "gazelle",
-  "gila monster",
-  "giraffe",
-  "gnu",
-  "goat",
-  "gopher",
-  "gorilla",
-  "grizzly bear",
-  "ground hog",
-  "guinea pig",
-  "hamster",
-  "hedgehog",
-  "hippopotamus",
-  "hog",
-  "horse",
-  "hyena",
-  "ibex",
-  "iguana",
-  "impala",
-  "jackal",
-  "jaguar",
-  "kangaroo",
-  "koala",
-  "lamb",
-  "lemur",
-  "leopard",
-  "lion",
-  "lizard",
-  "llama",
-  "lynx",
-  "mandrill",
-  "marmoset",
-  "mink",
-  "mole",
-  "mongoose",
-  "monkey",
-  "moose",
-  "mountain goat",
-  "mouse",
-  "mule",
-  "muskrat",
-  "mustang",
-  "mynah bird",
-  "newt",
-  "ocelot",
-  "opossum",
-  "orangutan",
-  "oryx",
-  "otter",
-  "ox",
-  "panda",
-  "panther",
-  "parakeet",
-  "parrot",
-  "pig",
-  "platypus",
-  "polar bear",
-  "porcupine",
-  "porpoise",
-  "prairie dog",
-  "puma",
-  "rabbit",
-  "raccoon",
-  "ram",
-  "rat",
-  "reindeer",
-  "reptile",
-  "rhinoceros",
-  "salamander",
-  "seal",
-  "sheep",
-  "shrew",
-  "silver fox",
-  "skunk",
-  "sloth",
-  "snake",
-  "squirrel",
-  "tapir",
-  "tiger",
-  "toad",
-  "turtle",
-  "walrus",
-  "warthog",
-  "weasel",
-  "whale",
-  "wildcat",
-  "wolf",
-  "wolverine",
-  "wombat",
-  "woodchuck",
-  "yak",
-  "zebra"
+const fruits = [
+        "apple",
+        "apricot",
+        "avocado",
+        "banana",
+        "bell pepper",
+        "bilberry",
+        "blackberry",
+        "blackcurrant",
+        "blood orange",
+        "blueberry",
+        "boysenberry",
+        "breadfruit",
+        "canary melon",
+        "cantaloupe",
+        "cherimoya",
+        "cherry",
+        "chili pepper",
+        "clementine",
+        "cloudberry",
+        "coconut",
+        "cranberry",
+        "cucumber",
+        "currant",
+        "damson",
+        "date",
+        "dragonfruit",
+        "durian",
+        "eggplant",
+        "elderberry",
+        "feijoa",
+        "fig",
+        "goji berry",
+        "gooseberry",
+        "grape",
+        "grapefruit",
+        "guava",
+        "honeydew",
+        "huckleberry",
+        "jackfruit",
+        "jambul",
+        "jujube",
+        "kiwi fruit",
+        "kumquat",
+        "lemon",
+        "lime",
+        "loquat",
+        "lychee",
+        "mandarine",
+        "mango",
+        "mulberry",
+        "nectarine",
+        "nut",
+        "olive",
+        "orange",
+        "papaya",
+        "passionfruit",
+        "peach",
+        "pear",
+        "persimmon",
+        "physalis",
+        "pineapple",
+        "plum",
+        "pomegranate",
+        "pomelo",
+        "purple mangosteen",
+        "quince",
+        "raisin",
+        "rambutan",
+        "raspberry",
+        "redcurrant",
+        "rock melon",
+        "salal berry",
+        "satsuma",
+        "star fruit",
+        "strawberry",
+        "tamarillo",
+        "tangerine",
+        "tomato",
+        "ugli fruit",
+        "watermelon"
 ];
 
-let currentAnimal = ``;
+
+let currentFruit = ``;
 let currentAnswer = ``;
+
+//for correct answers
+let correctMaxScore = 5;
+let currentScore = 0;
+
+//for wrong answers
+let currentLives = 5
+let livesLeft = 0;
+
+
+let state = `start`;
 
 function preload() {
 
@@ -157,7 +121,7 @@ function setup() {
 
   if(annyang){
     let commands = {
-      'I think it is *animals': guessAnimal
+      'I think it is *fruits': guessFruit
     };
     annyang.addCommands(commands);
     annyang.start();
@@ -171,9 +135,9 @@ function setup() {
 
 
 function draw() {
-  background(0);
+  stateChange();
 
-  if(currentAnswer === currentAnimal){
+  if(currentAnswer === currentFruit){
     fill(0, 255, 0);
   }
   else{
@@ -182,15 +146,35 @@ function draw() {
   text(currentAnswer, width/2, height/2);
 }
 
+function stateChange(){
+  if (state === `start`){
+    start();
+  }
+  else if(state === `instructions`){
+    instructions();
+  }
+  else if(state === `game`){
+    game();
+
+  }
+  else if(state === `win`){
+    win();
+  }
+
+  else if(state === `lose`){
+    lose();
+  }
+}
+
 function mousePressed(){
-  currentAnimal = random(animals); //chooses a random animal
-  let reverseAnimal = reverseString(currentAnimal); //reserves the current animal
-  responsiveVoice.speak(reverseAnimal);
+  currentAnimal = random(fruits); //chooses a random fruit
+  let reverseFruit = reverseString(currentFruit); //reserves the current Fruit
+  responsiveVoice.speak(reverseFruit);
 
 }
 
-function guessAnimal(animal){
-  currentAnswer = animal.toLowerCase();
+function guessFruit(fruit){
+  currentAnswer = fruit.toLowerCase();
   console.log(currentAnswer);
 }
 
