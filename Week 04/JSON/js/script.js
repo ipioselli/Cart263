c4233aa0eb68ecb20740c27cf754024eb8faf848
ipioselli <ines.pioselli@gmail.com>
2022-01-29 23:24:24 -0500

    W4: added mousePressed video 4.1

--- Week 04/JSON/js/script.js
+++ Week 04/JSON/js/script.js
@@ -1,33 +1,48 @@
 /**
-Title of Project
-Author Name
+4.1 intro to JSON
+Ines Pioselli
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+
 */
 
 "use strict";
 
+let taroData = undefined;
+let fortune = `No fortune found yet`;
 
-/**
-Description of preload
-*/
+//loads everything
 function preload() {
-
+  //taroData = loadJSON(`assets/data/tarot_interpretations.json`);
 }
 
 
-/**
-Description of setup
-*/
 function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+
 
 }
 
 
-/**
-Description of draw()
-*/
 function draw() {
+  background(255);
+  // let firstShadowMeaning = taroData.tarot_interpretations[0].meanings.shadow[0];
 
+  push();
+  textSize(32);
+  textAlign(CENTER);
+  fill(0);
+  text(fortune, width /2, height/2);
+  pop();
+}
+
+//when you click it gives you a random fortune
+function mousePressed(){
+  loadJSON(`assets/data/tarot_interpretations.json`, tarotLoaded) //same as preload
+}
+
+function tarotLoaded(data){
+  taroData = data;
+  let card  = random(taroData.tarot_interpretations);
+  fortune = random(card.fortune_telling);
 }
