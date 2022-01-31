9e842ab6fe1d6ce68990740522b9109143e6e707
ipioselli <ines.pioselli@gmail.com>
2022-01-30 21:44:40 -0500

    W4: Finished video 4.1

--- Week 04/JSON/js/script.js
+++ Week 04/JSON/js/script.js
@@ -12,12 +12,16 @@ let fortune = `No fortune found yet`;
 
 //loads everything
 function preload() {
-  //taroData = loadJSON(`assets/data/tarot_interpretations.json`);
+  // taroData = loadJSON(`assets/data/tarot_interpretations.json`);
+  taroData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/tarot_interpretations.json`); //same as loading method above but uses a url instead
 }
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+    let card  = random(taroData.tarot_interpretations);
+    fortune = random(card.fortune_telling);
 
 
 
@@ -26,7 +30,7 @@ function setup() {
 
 function draw() {
   background(255);
-  // let firstShadowMeaning = taroData.tarot_interpretations[0].meanings.shadow[0];
+  // let firstShadowMeaning = taroData.tarot_interpretations[0].meanings.shadow[0]; //gives you first sentence in the array at 0
 
   push();
   textSize(32);
@@ -36,13 +40,13 @@ function draw() {
   pop();
 }
 
-//when you click it gives you a random fortune
-function mousePressed(){
-  loadJSON(`assets/data/tarot_interpretations.json`, tarotLoaded) //same as preload
-}
+// //when you click it gives you a random fortune
+// function mousePressed(){
+//   loadJSON(`assets/data/tarot_interpretations.json`, tarotLoaded) //same as preload
+// }
 
-function tarotLoaded(data){
-  taroData = data;
-  let card  = random(taroData.tarot_interpretations);
-  fortune = random(card.fortune_telling);
-}
+// function tarotLoaded(data){
+//   taroData = data;
+//   let card  = random(taroData.tarot_interpretations);
+//   fortune = random(card.fortune_telling);
+// }
