d4721b7ab44da1692f8afe61164f880d5ac95850
ipioselli <ines.pioselli@gmail.com>
2022-02-02 17:17:46 -0500

    A4: Loaded JSON files

--- Activity/Spy Profile Generator/js/script.js
+++ Activity/Spy Profile Generator/js/script.js
@@ -8,26 +8,54 @@ author, and this description to match your project!
 
 "use strict";
 
+let spyProfile = {
+  name: `**REDACTED**`,
+  alias: `**REDACTED**`,
+  secretWeapon: `**REDACTED**`,
+  passWord: `**REDACTED**`
+};
 
-/**
-Description of preload
-*/
-function preload() {
+let instrumentData = undefined;
+let objectData =  undefined;
+let taroData = undefined;
 
+function preload(){
+
+  instrumentData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/music/instruments.json`);
+
+  objectData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/objects/objects.json`);
+
+  taroData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/tarot_interpretations.json`);
 }
 
 
-/**
-Description of setup
-*/
+
+
 function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  
+
 
 }
 
+function generateSpyProfile(){
+  spyProfile.name = prompt(`Agent! What is your name?!`);
+}
 
-/**
-Description of draw()
-*/
 function draw() {
 
+background(255);
+
+let profile = `** SPY PROFILE! DO NOT DISTRIBUTE! ** Name: ${spyProfile.name}
+Alias: ${spyProfile.name}
+Secret Weapon: ${spyProfile.secretWeapon}
+Password: ${spyProfile.passWord}`;
+
+push();
+textFont(`Courier, monospace`);
+textSize(24);
+textAlign(LEFT, TOP);
+text(profile, 100, 100);
+pop();
 }
