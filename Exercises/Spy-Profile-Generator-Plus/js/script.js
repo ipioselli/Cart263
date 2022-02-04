b744d0705966358d654cbb35e4d26da2f3d07293
ipioselli <ines.pioselli@gmail.com>
2022-02-03 23:21:40 -0500

    E3:p Updating game state

--- Exercises/Spy-Profile-Generator-Plus/js/script.js
+++ Exercises/Spy-Profile-Generator-Plus/js/script.js
@@ -19,14 +19,12 @@ magicalGirlProfile = spyProfile
 let magicalGirlProfile = {
   name: `**TOP SECRET**`,
   alias: `**TOP SECRET**`,
-  zodiac: `**TOP SECRET**`,
   powers:`**TOP SECRET**`,
   magicWand: `**TOP SECRET**`,
   birthPlace: `**TOP SECRET**`,
   password: `**TOP SECRET**`,
 
   sidekickName: `**TOP SECRET**`,
-  sidekickZodiac: `**TOP SECRET**`,
   sidekickPowers:`**TOP SECRET**`,
   sidekickBirthPlace:`**TOP SECRET**`,
 
@@ -48,17 +46,18 @@ let gameBg;
 
 let state = `start`;
 
+
 function preload(){
 
   loveCraftData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/mythology/lovecraft.json`);
   streetFighterData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/games/street_fighter_ii.json`);
   zodiacData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/zodiac.json`);
-  artifactData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/archetypes/artifact.json`);
-  planetData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/science/planets.json`);
+  artifactData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/objects/objects.json`);
+  planetData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/science/minor_planets.json`);
   gemstoneData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/materials/gemstones.json`);
 
-  startBg = loadImage(`assets/images/start-Bg`);
-  startFont = loadFont(`assets/fonts/Chromate-Regular`);
+  startBg = loadImage("assets/images/start-Bg.png");
+  startFont = loadFont("assets/fonts/Chromate-Regular.otf");
 
 
 
@@ -71,42 +70,45 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 
   let data = JSON.parse(localStorage.getItem(`magical-girl-profile-data`));
-
-  if(data !==null){
-    let password = prompt(`Please enter your password.`);
-    if(password === data.password){
-      magicalGirlProfile.name = data.name;
-      magicalGirlProfile.alias = data.alias;
-      magicalGirlProfile.zodiac = data.zodiac;
-      magicalGirlProfile.powers = data.powers;
-      magicalGirlProfile.magicWand = data.magicWand;
-      magicalGirlProfile.birthPlace = data.birthPlace;
-      magicalGirlProfile.password = data.password;
-
-      magicalGirlProfile.sidekickName = data.sidekickName;
-      magicalGirlProfile.sidekickZodiac = data.sidekickZodiac;
-      magicalGirlProfile.sidekickPowers = data.sidekickPowers;
-      magicalGirlProfile.sidekickBirthPlace = data.sidekickBirthPlace;
+    // if(data){
+    //   let password = prompt(`Please enter your password.`);
+    //   if(password === data.password){
+    //     loadMagicalGirlData();
+    //     loadSidekickData();
+    //   }
+    // }
+    // else{
+      generateMagicalGirlProfile();
     }
-  }
-  else{
-    generateMagicalGirlProfile();
-  }
+
+
+function loadMagicalGirlData(){
+  magicalGirlProfile.name = data.name;
+  magicalGirlProfile.alias = data.alias;
+  magicalGirlProfile.powers = data.powers;
+  magicalGirlProfile.magicWand = data.magicWand;
+  magicalGirlProfile.birthPlace = data.birthPlace;
+  magicalGirlProfile.password = data.password;
+}
+
+function loadSidekickData(){
+  magicalGirlProfile.sidekickName = data.sidekickName;
+  magicalGirlProfile.sidekickPowers = data.sidekickPowers;
+  magicalGirlProfile.sidekickBirthPlace = data.sidekickBirthPlace;
 }
 
 function generateMagicalGirlProfile(){
   magicalGirlProfile.name = prompt(`Welcome! What is your name?`);
   magicalGirlProfile.alias =  random(loveCraftData.deities);
-  magicalGirlProfile.zodiac = random(zodiacData.western_zodiac);
 
-  let card = random(streetFighterData.characters);
-  magicalGirlProfile.powers = random(card.moves);
+  let power01 = random(streetFighterData.characters);
+  magicalGirlProfile.powers = random(power01.moves);
 
-  let wand = random(artifactData.artifacts);
-  magicalGirlProfile.magicWand =  random(wand.name);
 
-  let planet = random(planetData.planets);
-  magicalGirlProfile.birthPlace = random(planet.name)
+  magicalGirlProfile.magicWand =  random(artifactData.objects);
+
+
+  magicalGirlProfile.birthPlace = random(planetData.minor_planets);
 
   magicalGirlProfile.password = random(gemstoneData.gemstones);
 
@@ -130,20 +132,8 @@ function generateSidekickProfile(){
 
 function draw() {
 changeState();
-// background(255);
-//
-// let profile = `** SPY PROFILE! DO NOT DISTRIBUTE! **
-// Name: ${spyProfile.name}
-// Alias: ${spyProfile.alias}
-// Secret Weapon: ${spyProfile.secretWeapon}
-// password: ${spyProfile.password}`;
-//
-// push();
-// textFont(`Courier, monospace`);
-// textSize(24);
-// textAlign(LEFT, TOP);
-// text(profile, 100, 100);
-// pop();
+
+
 }
 
 function changeState(){
@@ -163,10 +153,12 @@ function start(){
   image(startBg, width/2, height/2, 1920, 1080);
   push();
   textFont(startFont);
-  textSize(30);
+  textSize(50);
   fill(255, 255, 255);
   textAlign(CENTER, CENTER);
   text(`Magical Girl Simulator!`, width/2, height/2 -100);
+  textSize(40);
+  text(`Press Enter to begin`, width/2, height/2 + 150);
   pop();
 
   sparkles();
@@ -177,11 +169,48 @@ function mission(){
 background(0);
 }
 
+function game(){
+  background(0);
+  let profile01 = `** Magical Girl Profile **
+  Name: ${magicalGirlProfile.name}
+  Magical Girl name: ${magicalGirlProfile.alias}
+  Powers: ${magicalGirlProfile.powers}
+  Zodiac Sign:  ${magicalGirlProfile.zodiac}
+  Magical Weapon: ${magicalGirlProfile.magicWand}
+  Birth Place: ${magicalGirlProfile.birthPlace}
+  password: ${magicalGirlProfile.password}`;
+
+  let profile02 = ` ** Sidekick Profile **
+  Name:  ${magicalGirlProfile.sidekickName}
+  Birth Place:  ${magicalGirlProfile.sidekickBirthPlace}`;
+
+
+
+  push();
+  textFont(`startFont`);
+  textSize(24);
+  fill(255);
+  textAlign(LEFT, TOP);
+  text(profile01, 100, 100);
+  text(profile02, 100, 500);
+  pop();
+}
+
 function sparkles(){
   for (let i = 0; i < 1000; i++) {
     let x = random(0, width);
     let y = random(0, height);
     stroke(400);
     point(x, y);
+  }
+}
+
+
+function keyPressed(){
+  if(state === `start` && keyCode === 13){
+    state = `mission`;
+  }
+  else if(state === `mission` && keyCode === 32){
+    state = `game`;
   }
 }
