550964ffb0a2ddb4bdd95ca6ee0eb6d6cdf36b51
ipioselli <ines.pioselli@gmail.com>
2022-02-11 14:24:29 -0500

    E4: Done commenting

--- Exercises/Bubble-Popper-Plus-Plus/js/script.js
+++ Exercises/Bubble-Popper-Plus-Plus/js/script.js
@@ -124,7 +124,7 @@ function changeState() {
   }
 }
 
-
+//start state
 function start() {
   imageMode(CENTER, CENTER);
   image(startBg, width / 2, height / 2, 640, 480);
@@ -137,9 +137,9 @@ function start() {
   textSize(30);
   text(`Press ENTER to start`, width / 2, height / 2 - 50);
   pop();
-
 }
 
+//instructions state
 function instructions() {
   imageMode(CENTER, CENTER);
   image(instructionsBg, width / 2, height / 2, 640, 480);
@@ -155,23 +155,26 @@ function instructions() {
   text(`Press SPACEBAR to continue`, width / 2, height / 2);
   pop();
 
-  sparkles();
+  sparkles(); //adds sparkle effects
 }
 
-
+//loading state
 function loading() {
   background(0);
 
+  // Start the Handpose model and switch to our game state when it loads
   handpose = ml5.handpose(video, {
     flipHorizontal: true //flips camera
   }, function() {
-    state = `game`
+    state = `game` //calls the game state
   });
-
+  // Listen for prediction events from Handpose and store the results in our
+  // predictions array when they occur
   handpose.on(`predict`, function(results) {
     predictions = results;
   });
 
+  //text
   push();
   textFont(titleFont);
   fill(255);
@@ -183,34 +186,40 @@ function loading() {
 
 }
 
+//game state
 function game() {
-imageMode(CENTER, CENTER);
-image(gameBg, width/2, height/2, 640, 480)
+  //background image
+  imageMode(CENTER, CENTER);
+  image(gameBg, width / 2, height / 2, 640, 480)
 
+  // Check if there currently predictions to display
   if (predictions.length > 0) {
+    // If yes, then get the positions of the tip and base of the index finger
     updatepin(predictions[0]);
 
+    //check if the tip of the pin is touching the angel
     let d = dist(pin.tip.x, pin.tip.y, angel.x, angel.y);
     if (d < angel.size / 2) {
-      angelKillSFX.play();
-      angelsKilled++;
-      angel.vy= angel.vy - 2; //speeds up by 2 everytime you hit it
-      angel.size = angel.size -5;//gets smaller by 5px whenever you hit it
+      angelKillSFX.play(); //plays the sfx
+      angelsKilled++; //counter goes up by 1
+      angel.vy = angel.vy - 2; //speeds up by 2 everytime you hit it
+      angel.size = angel.size - 5; //gets smaller by 5px whenever you hit it
 
-      resetAngel();
+      resetAngel(); //resets angel position
     }
-    displaypin();
+    displaypin(); //displays the pin
   }
 
-  checkScore();
-  checkTimer();
-  moveAngel();
-  checkOutofBounds();
-  displayAngel();
-  displayScore();
-  displayTimer();
+  checkScore(); //calls function to check the score
+  checkTimer(); //calls function to check the timer
+  moveAngel(); //function to move the angel
+  checkOutofBounds(); //check if the angel is outside of the canvas
+  displayAngel(); //displays the angel
+  displayScore(); //displays the score
+  displayTimer(); //displays the timer
 }
 
+//function for the win state
 function win() {
   background(0);
 
@@ -224,13 +233,16 @@ function win() {
   text(`Press R to restart the game`, width/2, height/2 );
   pop();
 
-  sparkles();
-  resetGame();
+  sparkles(); //sparkle effects
+  resetGame(); //resets the game variables after reloading
 }
 
+//function for the lose state
 function lose(){
+  //background image
   imageMode(CENTER, CENTER);
   image(loseBg, width/2, height/2, 640, 480);
+  //text
   push();
   fill(255);
   textFont(titleFont);
@@ -243,25 +255,26 @@ function lose(){
 
 }
 
+//function to check the score
 function checkScore() {
   if (angelsKilled === maxAngelsKilled) {
-    state = `win`;
+    state = `win`; //if the score = 7 win state is called
   }
 }
 
 //calls the lose state once the timer is over
 function checkTimer() {
-  timer -= 1;
+  timer -= 1; //timer goes down by 1
   if (timer <= 0) {
-    timer = true;
+    timer = true; //changes timer to true when it reaches 0
   }
-
+  
   if (timerDone) {
-    state = `lose`;
+    state = `lose`; //calls the lose state once its true
   }
 }
 
-
+//function to add sparkling effect
 function sparkles() {
   for (let i = 0; i < 1000; i++) {
     let x = random(0, width);
@@ -271,6 +284,7 @@ function sparkles() {
   }
 }
 
+//Updates the position of the pin according to the latest prediction
 function updatepin(prediction) {
   pin.tip.x = prediction.annotations.indexFinger[3][0];
   pin.tip.y = prediction.annotations.indexFinger[3][1];
@@ -278,43 +292,48 @@ function updatepin(prediction) {
   pin.head.y = prediction.annotations.indexFinger[0][1];
 }
 
+//resets angel position
 function resetAngel() {
   angel.x = random(width);
   angel.y = height;
 }
 
+//resets angel velocity
 function moveAngel() {
   angel.x += angel.vx;
   angel.y += angel.vy;
 }
 
+//checks if the angel is off the canvas
 function checkOutofBounds() {
   if (angel.y < 0) {
     resetAngel();
   }
 }
 
+//reset game variables
 function resetGame(){
   angelsKilled = 0;
   timer = 5000;
 }
 
+//displays the angel with an image
 function displayAngel() {
   imageMode(CENTER, CENTER);
-
   image(angelImg, angel.x, angel.y, angel.size, angel.size);
 
 }
 
-
-
+//function to display the pin
 function displaypin() {
+  //pin tip
   push();
   stroke(255, 0, 0);
   strokeWeight(4);
   line(pin.tip.x, pin.tip.y, pin.head.x, pin.head.y);
   pop();
 
+  //pin head
   push();
   fill(255, 0, 0);
   noStroke();
@@ -322,6 +341,7 @@ function displaypin() {
   pop();
 }
 
+//displays the score
 function displayScore() {
   push();
   textSize(50);
@@ -331,6 +351,7 @@ function displayScore() {
   pop();
 }
 
+//displays the timer
 function displayTimer() {
   push();
   textSize(50);
@@ -340,7 +361,7 @@ function displayTimer() {
   pop();
 }
 
-
+//handles keyboard input
 function keyPressed() {
   if (state === `start`) {
     if (keyCode === 13) { //keycode for enter
@@ -354,18 +375,15 @@ function keyPressed() {
       instructionsSong.stop();
       video = createCapture(VIDEO);
       video.hide();
-
     }
   }
-
   if(state === `win`){
-    if(keyCode === 82){
+    if(keyCode === 82){ //keycode for R
       state = `start`;
     }
   }
-
   if(state === `lose`){
-    if(keyCode === 82){
+    if(keyCode === 82){ //keycode for R
       state = `start`;
     }
   }
