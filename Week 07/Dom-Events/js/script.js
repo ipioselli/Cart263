c05d912345ae8286be34b9cf693c1c3b95f52e0d
ipioselli <ines.pioselli@gmail.com>
2022-02-18 18:47:14 -0500

    W6: mouse events

--- Week 07/Dom-Events/js/script.js
+++ Week 07/Dom-Events/js/script.js
@@ -1,23 +1,59 @@
 //DOM events
+//video 6.4
 
 let paragraph = document.getElementById(`paragraph`);
+let originalText = paragraph.innerText;
 
-let opacity = 1;
-fadeOut();
 
-function fadeOut(){
-  opacity -= 0.01;
-  paragraph.style[`opacity`] = opacity;
+////hover over with mouse
+// paragraph.addEventListener(`mouseenter`, function(event){
+//   event.target.innerText = `SECRET MESSAGE`;
+// });
 
-  if(opacity > 0){
-    requestAnimationFrame(fadeOut);
-  }
-}
+paragraph.addEventListener(`contextmenu`, function(event){
+  event.target.innerText = `SECRET MESSAGE`;
+});
+
+paragraph.addEventListener(`mouseleave`, function(event){
+  event.target.innerText = originalText;
+});
+
+
+
+// let mainHeading = document.getElementById(`main-heading`);
+// let subHeading = document.getElementById(`sub-heading`);
+//
+// mainHeading.addEventListener(`click`, setRedTextColor);
+// subHeading.addEventListener(`click`, setRedTextColor);
+// paragraph.addEventListener(`click`, setRedTextColor);
+//
+// function setRedTextColor(event){
+//   event.target.style[`color`] = `#ff0000`;
+// }
+
+
+
+
+
+// //----------------------//
+// //--- Opacity effect ---//
+// //----------------------//
+// let opacity = 1;
+// fadeOut();
+//
+// function fadeOut(){
+//   opacity -= 0.01;
+//   paragraph.style[`opacity`] = opacity;
+//
+//   if(opacity > 0){
+//     requestAnimationFrame(fadeOut);
+//   }
+// }
 
 // //------------------------------//
 // //--- Color changing effect ---//
 // //----------------------------//
-//changes the color to red after 3 secs
+// //changes the color to red after 3 secs
 // setTimeout(function(){
 //   paragraph.style[`color`] = `#ff0000`;
 //   }, 3000);
