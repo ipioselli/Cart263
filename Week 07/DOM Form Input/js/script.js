7331eb796f546223126288b70d2bca59348ddb6e
ipioselli <ines.pioselli@gmail.com>
2022-02-20 21:40:59 -0500

    A5: finished video for haiku generator

---
+++ Week 07/DOM Form Input/js/script.js
@@ -0,0 +1,13 @@
+let button = document.getElementById(`example-button`);
+
+
+// //displays an alert when clicked
+// button.addEventListener(`click`, function(event){
+//   alert.(`Nice clicking!`);
+// });
+
+
+// //makes the button disappear when clicked
+// button.addEventListener(`click`, function(event){
+// event.target.style[`display`] = `none`;
+// });
