ae154f9196cfc10f79f22ac37414f204c6b98b78
ipioselli <ines.pioselli@gmail.com>
2022-02-22 23:14:42 -0500

    P1: Added a veggies class for bouncing veggies

---
+++ Projects/Project 01/Project 01/js/Veggies.js
@@ -0,0 +1,54 @@
+class Veggies {
+  constructor(x,y, image){
+    this.x = x;
+    this.y = y;
+    this.vy = vy;
+    this.vx = vx;
+
+    this.ax = 0;
+    this.ay= 0;
+    this.maxSpeed = 10;
+    this.size = 50;
+    this.image = image;
+  }
+
+
+  update(){
+    this.display();
+    this.move();
+  }
+
+  move(){
+
+    this.vx = this.vx + this.ax;
+    this.vy = this.vy + this.ay;
+
+    //constrain the velocity
+    this.vx = constrain(this.vx, -this.maxSpeed, this.maxSpeed);
+    this.vy = constrain(this.vy, -this.maxSpeed, this.maxSpeed);
+
+    //update the position with velocity
+    this.x = this.x + this.vx;
+    this.y = this.y + this.vy;
+
+    //constrains the ball to the canvas
+    this.x = constrain(this.x, 0, width);
+    this.y = constrain(this.y, 0, height);
+
+    //makes the ball bounce off the canvas
+    if (this.x >= width || this.x <= 0) {
+      this.vx = -this.vx;
+    }
+    if (this.y >= width || this.y <= 0) {
+      this.vy = -this.vy;
+    }
+
+  }
+
+  display(){
+    push();
+    noStroke();
+    image(this.image, this.x, this.y, this.size, this.size);
+    pop();
+  }
+}
