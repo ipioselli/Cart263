c540c86eda672848b7c870632fd097cac2cb3fa9
ipioselli <ines.pioselli@gmail.com>
2022-03-14 15:18:09 -0400

    E6: Added twilight text and adding spans everywhere

--- Exercises/Raving-Redactionist-Plus-Plus/plain-javascript-project/js/script.js
+++ Exercises/Raving-Redactionist-Plus-Plus/plain-javascript-project/js/script.js
@@ -2,27 +2,44 @@
 Exercise 6: Raving Redactionist
 Ines Pioselli
 
+Brief:
+- improve visuals
+- find a different text
+- animate
+- secret message
+
 */
 
 "use strict";
+//fades in the secret document when the button is clicked
+$(`#button`).on(`click`, function(event){
+  $(`#secret-document`).fadeIn();
+});
 
-$(`.top-secret`).on(`click`, redact);
-setInterval(revelation, 500);
+$(`.leaving`).one(`mouseenter`, function(event) {
+  $(this).fadeOut(`leave`) ;
+});
+// $(`.top-secret`).on(`click`, redact);
+// setInterval(revelation, 500);
 
-function redact(event){
-  $(this).removeClass(`revealed`);
-  $(this).addClass(`redacted`);
-}
+$(`.grower`).one(`mouseover`, function(event) {
+      $(this).addClass(`grow`);
+});
 
-function revelation(){
-  $(`.redacted`).each(attemptReveal);
-}
-
-
-function attemptReveal(){
-  let r = Math.random();
-  if(r < 0.1){ //10% of the time
-    $(this).removeClass(`redacted`);
-    $(this).addClass(`revealed`);
-  }
-}
+// function redact(event){
+//   $(this).removeClass(`revealed`);
+//   $(this).addClass(`redacted`);
+// }
+//
+// function revelation(){
+//   $(`.redacted`).each(attemptReveal);
+// }
+//
+//
+// function attemptReveal(){
+//   let r = Math.random();
+//   if(r < 0.1){ //10% of the time
+//     $(this).removeClass(`redacted`);
+//     $(this).addClass(`revealed`);
+//   }
+// }
