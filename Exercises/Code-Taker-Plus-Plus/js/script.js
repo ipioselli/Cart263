46814e7085e4b6ddb4e13ea8981df422af7f0c82
ipioselli <ines.pioselli@gmail.com>
2022-03-17 20:26:38 -0400

    E7: added sortable property

--- Exercises/Code-Taker-Plus-Plus/js/script.js
+++ Exercises/Code-Taker-Plus-Plus/js/script.js
@@ -5,12 +5,13 @@ Ines Pioselli
 Brief:
 - rework the html and class
 - add a model instructions dialog
+- add a sound
 
 */
 
 "use strict";
 
-let message = `sal`;
+let message = `sailormoon`;
 
 let specialSound = new Audio(`assets/sounds/SailorMoon.mp3`);
 
@@ -41,17 +42,20 @@ $(`.secret`).one(`mouseover`, function(event){
 $(`#answer`).droppable({
   drop: function(event, ui){
     let letter = ui.draggable.text();
+    let span = $(`<span class = "answer-letter">${letter}</span>`);
     //adds it to the letter
-    $(this).append(letter);
+    $(this).append(span);
     ui.draggable.draggable(`disable`);
     ui.draggable.removeClass(`found`);
+    ui.draggable.off(`mouseover`);
     //check if its right
     if($(this).text() === message){
       //plays a sound when you get the answer right
       specialSound.play();
       $(`#solved-dialog`).dialog(`open`);
 
-
     }
   }
 });
+
+$(`answer`).sortable();
