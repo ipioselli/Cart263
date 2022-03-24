8b2c55d97f885e75b9ef6c34e3bde1fdd79ad0ee
ipioselli <ines.pioselli@gmail.com>
2022-03-24 11:37:46 -0400

    P2: Adding egg states

--- Projects/Project 02/Prototype/js/script.js
+++ Projects/Project 02/Prototype/js/script.js
@@ -1,27 +1,46 @@
 /**
-Project 2 Prototype
-Long Leg Fishy Dating Simulator
+Project 2 prototype
 Ines Pioselli
-
-This is a prototype of my love simulator. The first few states show examples of how the style will look.
-The minigame is a very rough and small scale of what will happen. The user will have an array of balls bouncing around and they must doge them and
-get to their fish lover to win their heart back. For now I just used 3 ellipses to show what will happen.
-
+Tamagotchi Sim
 
 */
 
 "use strict";
 
 let tamagotchiMenu = { // button to access the tutorial state
-  x:1280/2,
-  y:720/2,
-  size:200,
+  x: 1280 / 2,
+  y: 720 / 2,
+  size: 300,
 }
 
 
 
-
 let state = `start`; // the prototype starts with the title state
+
+let tamagotchiEgg;
+let newTamagotchiEgg;
+let egg01 = {
+  x: 1280 / 2,
+  y: 720 / 2,
+  size: 300,
+}
+let egg02 = {
+  x: 1280 / 2,
+  y: 720 / 2,
+  size: 300,
+}
+let egg03 = {
+  x: 1280 / 2,
+  y: 720 / 2,
+  size: 300,
+}
+
+let tamagotchiEnergy = 2000;
+let tamagotchiLVL = 1;
+
+let tamagotchi;
+
+let chooseEggBG;
 
 let pixelFont;
 
@@ -31,6 +50,11 @@ Description of preload
 function preload() {
   tamagotchiMenu.image = loadImage("assets/images/tamagotchi.png");
   pixelFont = loadFont(`assets/fonts/dogica.otf`);
+
+  chooseEggBG = loadImage(`assets/images/chooseEggBG.png`);
+  egg01.image = loadImage(`assets/images/egg01.png`);
+  egg02.image = loadImage(`assets/images/egg02.png`);
+  egg03.image = loadImage(`assets/images/egg03.png`);
 }
 
 
@@ -46,59 +70,101 @@ function setup() {
 
 //Draws all the states for the game
 function draw() {
-  if(state === `start`){
+  if (state === `start`) {
     start();
-  }
-  else if(state === `menu`){
+  } else if (state === `menu`) {
     menu();
+  }
+  else if(state === `chooseEgg`){
+    chooseEgg();
   }
 
 }
 
 
 //title state : homepage
-function start(){
-
+function start() {
 
   push();
-  background(64, 175, 222);
+  background(186, 219, 205);
   textFont(pixelFont);
   textAlign(CENTER, CENTER);
   textSize(50);
   fill(255, 255, 255);
-  text(`Press ENTER to start!`, width / 2 + 50, height / 2+350);
+  text(`ENTER to start!`, width / 2, height / 2);
 
   pop();
 
 }
 
 
-//When you hover over the tutorial button it will bring you to the tutorial state
-function menu(){
+
+function menu() {
   push();
-  background(64, 175, 222);
+  background(186, 219, 205);
   textAlign(CENTER, CENTER);
-    textFont(pixelFont);
-    fill(255);
-    textSize(80);
-    text(`Press spacebar`, width / 2, height / 2);
+  textFont(pixelFont);
+  fill(255);
+  textSize(50);
+  text(`Press spacebar`, width / 2, height / 2 + 200);
   pop();
   displayTamagotchiMenu();
 }
 
+function chooseEgg(){
+  imageMode(CENTER, CENTER);
+  image(chooseEggBG, width/2, height/2, 1280, 720);
+  push();
+  pop();
+}
 
-//displays the first page with the main character
+function livingRoom(){
+  
+}
+
+function displayTamagotchiEgg(){
+
+}
+
+
 function displayTamagotchiMenu() {
+  imageMode(CENTER, CENTER);
   image(tamagotchiMenu.image, tamagotchiMenu.x, tamagotchiMenu.y, tamagotchiMenu.size, tamagotchiMenu.size);
+  //jitter the tamagotchi
+  tamagotchiMenu.x = tamagotchiMenu.x + random(-1, 1);
 }
 
 //keyboard input
-function keyPressed(){
-  if(state === `start`){
-    if (keyCode === 13){ //keycode for ENTER
+function keyPressed() {
+  if (state === `start`) {
+    if (keyCode === 13) { //keycode for ENTER
       state = `menu`;
     }
   }
+  if(state === `menu`){
+    if(keyCode === 32){
+      state = `chooseEgg`;
+    }
+  }
+  if(state === `chooseEgg`){
+      if(keyCode === 49){
+        tamagotchiEgg === egg01;
+        state = `livingRoom`;
+      }
+  }
+  if(state === `chooseEgg`){
+      if(keyCode === 50){
+        tamagotchiEgg === egg02;
+        state = `livingRoom`;
+      }
+  }
+  if(state === `chooseEgg`){
+      if(keyCode === 51){
+        tamagotchiEgg === egg03;
+        state = `livingRoom`;
+      }
+  }
+
 
 
 
