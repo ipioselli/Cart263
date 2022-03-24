e661b96919d305f26f026ce0e637ac68d02e618f
ipioselli <ines.pioselli@gmail.com>
2022-03-24 10:05:11 -0400

    P2: Adding start and menu states

--- Projects/Project 02/Prototype/js/script.js
+++ Projects/Project 02/Prototype/js/script.js
@@ -11,9 +11,23 @@ Tamagotchi simulator prototype
 let evolutionlvl= 1;
 let evolutions = 3;
 
-let state = `livingroom`;
+let state = `start`;
+
+let tamagotchiMenu = {
+  x: 1280/2,
+  y: 720/2,
+  size: 50,
+}
+
+//fonts
+let pixelFont;
+let pixelFontBold;
 
 function preload() {
+
+  pixelFont = loadFont(`assets/fonts/dogica.otf`);
+
+  tamagotchiMenu.image = loadImage(`assets/images/tamagotchi.png`);
 
 }
 
@@ -21,12 +35,14 @@ function preload() {
 
 function setup() {
 
+  createCanvas(1280, 720);
+
 }
 
 
 
 function draw() {
-
+  setupStates();
 }
 
 
@@ -36,5 +52,29 @@ function setupStates(){
   }
   else if(state === `menu`){
     menu();
+  }
+}
+
+
+function start(){
+  push();
+  textAlign(CENTER, CENTER);
+  textFont(pixelFont);
+  fill(255);
+  textSize(80);
+  text(`ENTER to begin`, width/2, height/2);
+  pop();
+}
+
+function menu(){
+
+}
+
+
+function keyPressed(){
+  if(state === `start`){
+    if(keyCode === 13){
+      state = `menu`;
+    }
   }
 }
