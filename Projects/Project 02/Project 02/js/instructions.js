4eabf66ac0f5eb80edb40b75bd8cf86dfdf8461d
ipioselli <ines.pioselli@gmail.com>
2022-04-21 23:23:12 -0400

    P2: added more comments

---
+++ Projects/Project 02/Project 02/js/instructions.js
@@ -0,0 +1,11 @@
+//instructions
+let tamagotchiInstructions = `You are given a lil italian coffee bean to raise! \n The lil bean gets a house to live in.\nYou must make sure its energy level stays up by feeding, \ncleaning and loving him.`;
+let tamagotchiInstructions02 = `At 12pm the tamagotchi must go to school. \nOnce it finishes school it will evolve into a new form!`;
+let feedInstructions = `Feed the tamagotchi by saying the name of the food. Hint The tamagotchi loves italian food`;
+let showerInstructions = `Press the letter S on the keypad to wash away all the dirt`;
+let sleepInstructions01 = `It's not time for bed yet. Your bedtime is at 9`;
+let sleepInstructions02 = `Time for bed`;
+let petInstructions = `Hover your index finger over the tamagotchi to pet it and increase the energy level`;
+let schoolInstructions01 = `It's your tamagotchi's first day of school. \n You must help it learn english. \nThe word's will be displayed in Italian and you must guess them in english. \n You can click on the english button for help \n Get 5 right to continue or if you get 5 wrong your tamagotchi will die >:(`;
+
+let schoolInstructions02 = `It's your tamagotchi's second day of school. \n You must help it learn english.\n The word's will be displayed in Italian and you must guess them in english. \n You can click on the english button for help`;
--- Projects/Project 02/Project 02/js/script.js
+++ Projects/Project 02/Project 02/js/script.js
@@ -4,6 +4,12 @@ Ines Pioselli
 
 Tamagotchi Sim
 
+left to do:
+- day 02 INSTRUCTIONS
+- day 03 INSTRUCTIONS
+- draw last tamagotchi
+- add comments and organize
+
 CAPPUGOTCHI
 - you are given a cute lil coffee bean as a pet.
 
@@ -16,6 +22,7 @@ CAPPUGOTCHI
 //main game variables
 let tamagotchiEgg;
 
+//all images for the tamagotchis emotions and evolutions
 let tamagotchiImg01;
 let tamagotchiImg02;
 let tamagotchiImg03;
@@ -26,8 +33,8 @@ let tamagotchiImg07;
 
 //header info
 let energyCounter = 0;
-let tamagotchiEnergy = 2000;
-let tamagotchiLVL = 1;
+let tamagotchiEnergy = 2000;//max energy amount
+let tamagotchiLVL = 1; //starts at evolution level 1
 let tamagotchi;
 let hour = 6; //day starts at 6am
 
@@ -112,19 +119,6 @@ let bedTimeTimerDelay = 700;
 let bedTimeTimerDone = false;
 
 
-
-//instructions
-let tamagotchiInstructions = `You are given a lil italian coffee bean to raise! \n The lil bean gets a house to live in.\nYou must make sure its energy level stays up by feeding, \ncleaning and loving him.`;
-let tamagotchiInstructions02 = `At 12pm the tamagotchi must go to school. \nOnce it finishes school it will evolve into a new form!`;
-let feedInstructions = `Feed the tamagotchi by saying the name of the food. Hint The tamagotchi loves italian food`;
-let showerInstructions = `Press the letter S on the keypad to wash away all the dirt`;
-let sleepInstructions01 = `It's not time for bed yet. Your bedtime is at 9`;
-let sleepInstructions02 = `Time for bed`;
-let petInstructions = `Hover your index finger over the tamagotchi to pet it and increase the energy level`;
-let schoolInstructions01 = `It's your tamagotchi's first day of school. \n You must help it learn english. \nThe word's will be displayed in Italian and you must guess them in english. \n You can click on the english button for help \n Get 5 right to continue or if you get 5 wrong your tamagotchi will die >:(`;
-
-let schoolInstructions02 = `It's your tamagotchi's second day of school. \n You must help it learn english.\n The word's will be displayed in Italian and you must guess them in english. \n You can click on the english button for help`;
-
 //background image variables
 
 let floorPlanBg;
@@ -193,8 +187,8 @@ function preload() {
   italianButton.image = loadImage(`assets/images/italianButton.png`);
 
   //load json data
-  englishData = loadJSON(`data/lesson_01.json`);
-  italianData = loadJSON(`data/lesson_01.json`);
+  englishData = loadJSON(`data/school_lesson.json`);
+  italianData = loadJSON(`data/school_lesson.json`);
 
   //load sounds
   song01 = loadSound(`assets/sounds/Cute.mp3`);
@@ -458,6 +452,7 @@ function checkLesson01Score(){
   if(state === `schoolDay01`){
     if(currentItalianAnswer === schoolLesson01.currentEnglishWord){
       schoolRightAnswers++;
+      blingSfx.play();
       nextQuestion();
 
       if(schoolRightAnswers === schoolMaxRightAnswers){
@@ -468,6 +463,7 @@ function checkLesson01Score(){
     }
     else if(currentItalianAnswer !== schoolLesson01.currentItalianWord){
       schoolWrongAnswers++;
+      badSfx.play();
       if(schoolWrongAnswers === schoolMaxWrongAnswers){
         state = `dead`;
       }
